311bb430637eb14bf632f12feb8433c080bb0605
ViktorSimko <aqviktor@gmail.com>
2017-09-19 12:24:46 +0200

    Initial Commit

---
+++ PokeMeKit/PokeMeKit.h
@@ -0,0 +1,19 @@
+//
+//  PokeMeKit.h
+//  PokeMeKit
+//
+//  Created by Simkó Viktor on 2017. 09. 19..
+//  Copyright © 2017. PokeMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokeMeKit.
+FOUNDATION_EXPORT double PokeMeKitVersionNumber;
+
+//! Project version string for PokeMeKit.
+FOUNDATION_EXPORT const unsigned char PokeMeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokeMeKit/PublicHeader.h>
+
+
