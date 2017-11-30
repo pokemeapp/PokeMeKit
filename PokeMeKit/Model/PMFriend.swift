c0d3f278b66da1b73f7b9c33543bf01b880de73d
ViktorSimko <aqviktor@gmail.com>
2017-11-30 21:06:53 +0100

    add friend model

---
+++ PokeMeKit/Model/PMFriend.swift
@@ -0,0 +1,18 @@
+//
+//  PMFriend.swift
+//  PokeMeKit
+//
+//  Created by Simkó Viktor on 2017. 11. 30..
+//  Copyright © 2017. PokeMe. All rights reserved.
+//
+
+import Foundation
+
+public class PMFriend: PMAPIEntity {
+  
+  public var id: Int?
+  public var userId: Int
+  public var friendId: Int
+  public var owner: PMUser?
+  
+}
