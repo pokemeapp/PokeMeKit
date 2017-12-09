c025766e81b2c3c0f0ccaf4be5d0b6615bccc07d
ViktorSimko <aqviktor@gmail.com>
2017-12-09 16:05:46 +0100

    add friend request to kit

---
+++ PokeMeKit/Model/PMFriendRequest.swift
@@ -0,0 +1,16 @@
+//
+// Created by Simkó Viktor on 2017. 12. 09..
+// Copyright (c) 2017 PokeMe. All rights reserved.
+//
+
+import Foundation
+
+public class PMFriendRequest: PMAPIEntity {
+
+  public var id: Int?
+  public var owner_id: Int?
+  public var target_id: Int?
+  public var status: Int?
+  public var owner: PMUser?
+
+}
