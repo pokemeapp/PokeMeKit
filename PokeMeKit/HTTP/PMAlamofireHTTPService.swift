9d07d7305d523fb1945d38b7b9140fd863544e74
ViktorSimko <aqviktor@gmail.com>
2017-12-11 22:53:04 +0100

    remove http stubs

--- PokeMeKit/HTTP/PMAlamofireHTTPService.swift
+++ PokeMeKit/HTTP/PMAlamofireHTTPService.swift
@@ -7,22 +7,12 @@
 //
 
 import Foundation
-import OHHTTPStubs
 import Alamofire
 
 public class PMAlamofireHTTPService: PMHTTPService {
   
-  public init() {
-    //initStubs()
-  }
-  
-  private func initStubs() {
-    stub(condition: isHost("localhost") && isPath("/api/user/friends")) { _ in
-      let stubPath = OHPathForFile("friends.json", type(of: self))
-      return fixture(filePath: stubPath!, headers: ["Content-Type": "application/json"])
-    }
-  }
-  
+  public init() {}
+
   public func request(_ urlRequest: URLRequest, _ callback: @escaping (Error?, HTTPURLResponse?, Data?) -> Void) {
 
     Alamofire.request(urlRequest).responseData { response in
