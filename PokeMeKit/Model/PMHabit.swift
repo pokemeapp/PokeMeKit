15fe1652070be810e96fde9e272da862cad2802d
ViktorSimko <aqviktor@gmail.com>
2017-12-11 21:28:01 +0100

    add habit

---
+++ PokeMeKit/Model/PMHabit.swift
@@ -0,0 +1,20 @@
+//
+// Created by Simkó Viktor on 2017. 12. 11..
+// Copyright (c) 2017 PokeMe. All rights reserved.
+//
+
+import Foundation
+
+public class PMHabit: PMAPIEntity {
+  
+  public init() { }
+  
+  public var id: Int?
+  public var user_id: Int?
+  public var type: String?
+  public var name: String?
+  public var description: String?
+  public var day: String?
+  public var hour: String?
+
+}
